4605c06af9fedd4b286fd865def817993f12d135
Rold-BOUDIMOU <rold@directransfert.com>
2026-07-28 17:29:51 +0100

    faste update

--- src/app/services/trip.service.ts
+++ src/app/services/trip.service.ts
@@ -3,7 +3,7 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Trip, TripSearchParams, TripResponse } from '../models';
 import { unwrapCollection } from '../shared/rxjs-operators';
-import { environment } from 'src/environments/environment.prod';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
